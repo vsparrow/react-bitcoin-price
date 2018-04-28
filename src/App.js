e77e5dd165f7916a3b68e08038eb99d7c706cbb6
vsparrow <khan@vsparrow.com>
2018-04-28 01:48:35 -0400

    alignment of Price in page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <div className="row">
-            <div className="jumbotron">
-              <h1 className="display-4 text-center">Hello, world!</h1>
-              <Price/>
-              <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-              <hr className="my-4"/>
-              <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
-              <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <div className="container ">
+          <div className="row align-items-center h-100">
+            <div className="col ">
+              <div className="jumbotron ">
+                <Price/>
+                <hr className="my-4"/>
+                <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+              </div>
             </div>
           </div>
         </div>
--- src/Price.js
+++ src/Price.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Price extends Component {
   constructor(props){
     super(props)
-    this.state = {}
+    this.state = {price: "", currency: "", timeUpdated: ""}
   }//constructor
   componentDidMount(){
     let url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
@@ -16,13 +16,23 @@ class Price extends Component {
     console.log(json.bpi.USD.rate);
     console.log(json.bpi.USD.code);
     console.log(json.time.updated);
+    this.setState({price: json.bpi.USD.rate,
+      currency: json.bpi.USD.code,
+      timeUpdated: json.time.updated
+    }) //setState
+
     //add GBP
     //add EUR
 
   }//convertJson
   render(){
     return(
-      <h1 className="display-4 text-center">Hello, Price!</h1>
+      <div>
+        <h1 className="display-4 text-center">Current Bitcoin Price:</h1>
+        <h1 className="display-4 text-center">${this.state.price} {this.state.currency}</h1>
+        <p className="lead text-center">Updated at: {this.state.timeUpdated}</p>
+
+      </div>
     )//return
   }//render
 }//class
