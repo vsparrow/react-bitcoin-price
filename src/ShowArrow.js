36b7ebd539faaaa93fcd5fb59de7a2b24e57ff46
vsparrow <khan@vsparrow.com>
2018-04-29 02:42:37 -0400

    not workiing at the moment

--- src/ShowArrow.js
+++ src/ShowArrow.js
@@ -6,15 +6,38 @@ class ShowArrow extends Component {
     this.state = {arrow: "fas fa-text-height"}
   }//constructor
 
-  componentWillReceiveProps(nextProps) {
+  // componentDidUpdate(prevProps, prevState){
+    //
+    // console.log("this.props" + this.props);
+    // console.log(this.props);
+    // console.log("state");
+    // console.log(this.state);
+    // let newFont = "blank"
+    // if (this.props.color === "green"){
+    //   // this.setState({arrow: "fas fa-arrow-alt-circle-up"})
+    //   console.log("GREEN");
+    //   newFont = 'fas fa-arrow-alt-circle-up'
+    // }
+    // else if (this.props.color === "red") {
+    //   console.log("RED");
+    //   newFont = 'fas fa-arrow-alt-circle-down'
+    // }
+    // else {
+    //   console.log("BLACK");
+    //   newFont = "blank"
+    // }
+    // this.setState({arrow: newFont})
+  // }
+  // componentWillReceiveProps(nextProps) {
+  static getDerivedStateFromProps(nextProps, prevState){
     // '<span><i className="fas fa-arrow-alt-circle-up"></i></span>'}
     // '<span><i className="fas fa-arrow-alt-circle-down"></i></span>
-    console.log("nextProps" + nextProps);
-    console.log(nextProps);
-    console.log("this.props" + this.props);
-    console.log(this.props);
-    console.log("state");
-    console.log(this.state);
+    // console.log("nextProps" + nextProps);
+    // console.log(nextProps);
+    // console.log("this.props" + this.props);
+    // console.log(this.props);
+    // console.log("state");
+    // console.log(this.state);
     //
     // let newFontIcon =  "???????????????"
     // // if (nextProps == "green"){newFontIcon =  "fa fa-arrow-alt-circle-up"}
@@ -29,10 +52,30 @@ class ShowArrow extends Component {
     // console.log(classu);
     // console.log(this.props);
     // return classu
-
+    let newFont = "blank"
+    if (nextProps === "green"){
+      // this.setState({arrow: "fas fa-arrow-alt-circle-up"})
+      console.log("GREEN");
+      newFont = 'fas fa-arrow-alt-circle-up'
+    }
+    else if (nextProps === "red") {
+      console.log("RED");
+      newFont = 'fas fa-arrow-alt-circle-down'
+    }
+    else {
+      console.log("BLACK");
+      newFont = "blank"
+    }
+    this.setState({arrow: newFont})
   }
   render(){
           // this.arrow
+
+          // <span>
+          // <i className={this.props.color === "black" ? "noColor":
+          //  {this.props.color === "green" ? 'fas fa-arrow-alt-circle-up' : 'fas fa-arrow-alt-circle-down'}></i>
+          //  }
+          // </span>
     return(
       <span>
       <i className={this.props.color === "green" ? 'fas fa-arrow-alt-circle-up' : 'fas fa-arrow-alt-circle-down'}></i>
